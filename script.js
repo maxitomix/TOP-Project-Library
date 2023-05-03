84fac2d7eb18b6fd161824bf0cc8b75c96dbce1a
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-05-03 09:37:05 -0400

    done

--- script.js
+++ script.js
@@ -21,81 +21,91 @@ let myLibrary = [
 const bookList = document.getElementById('book-list');
 
 function displayBooks() {
-  // clear the table body before repopulating it
-  bookList.innerHTML = '';
+    // clear the table body before repopulating it
+    bookList.innerHTML = '';
 
-  // create a table header row
-  const tableHeader = document.createElement('tr');
-  tableHeader.innerHTML = `
-   <th>Index</th>  
-   <th>Title</th>
-    <th>Author</th>
-    <th>Pages</th>
-    <th>isRead?</th>
-  `;
-  bookList.appendChild(tableHeader);
+    // create a table header row
+    const tableHeader = document.createElement('tr');
+    tableHeader.innerHTML = `
+    <th>Index</th>  
+    <th>Title</th>
+      <th>Author</th>
+      <th>Pages</th>
+      <th>isRead?</th>
+    `;
+    bookList.appendChild(tableHeader);
 
-  // loop through each book in the myLibrary array and create a table row for it
-  myLibrary.forEach(book => {
-    const bookRow = document.createElement('tr');
-    bookRow.innerHTML = `
-      <td>${myLibrary.indexOf(book)}</td>
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.pages}</td>
-      <td>${book.isRead ? 'Yes' : 'No'}</td>
-      <td><button class="delete-book-btn" data-index="${myLibrary.indexOf(book)}">Delete</button></td>
-      
-         `;
-    bookList.appendChild(bookRow);
-  });
+    // loop through each book in the myLibrary array and create a table row for it
+    myLibrary.forEach(book => {
+      const bookRow = document.createElement('tr');
+      bookRow.innerHTML = `
+        <td>${myLibrary.indexOf(book)}</td>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td><input type="checkbox" class="read-checkbox" ${book.isRead ? 'checked' : ''} data-index="${myLibrary.indexOf(book)}"></td>
+        <td><button class="delete-book-btn" data-index="${myLibrary.indexOf(book)}">Delete</button></td>
+        
+          `;
+      bookList.appendChild(bookRow);
+    });
+  
+    //delete button:
+    const deleteBtns = document.querySelectorAll('.delete-book-btn');
+    deleteBtns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        const libraryIndex = btn.dataset.index;
+        myLibrary.splice(libraryIndex,1);
+        displayBooks();
+      })
+    })
+  
 
-  //delete button:
-const deleteBtns = document.querySelectorAll('.delete-book-btn');
-deleteBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const libraryIndex = btn.dataset.index;
-    myLibrary.splice(libraryIndex,1);
-    displayBooks();
+  //save checkbox changes:
+  const checkbox = document.querySelectorAll('.read-checkbox');
+  checkbox.forEach(box => {
+    box.addEventListener('click', () => {
+      const libraryIndex = box.dataset.index;
+      myLibrary[libraryIndex].isRead = !myLibrary[libraryIndex].isRead;
+      displayBooks();
+    })
   })
-})
 }
 
+  //add new book button toggles display of for to add new book.
+  const addBookBtn = document.getElementById('add-book-btn');
+  const addBookForm = document.getElementById('add-book-form');
+  addBookBtn.addEventListener('click', () => {
+    addBookForm.classList.toggle('hidden')
+  })
 
-//add new book button toggles display of for to add new book.
-const addBookBtn = document.getElementById('add-book-btn');
-const addBookForm = document.getElementById('add-book-form');
-addBookBtn.addEventListener('click', () => {
-  addBookForm.classList.toggle('hidden')
-})
+  //add new book from form to library
+  const submitBookBtn = document.getElementById('submit-book-btn');
+  submitBookBtn.addEventListener('click',()=>{
+    event.preventDefault();
 
-//add new book from form to library
-const submitBookBtn = document.getElementById('submit-book-btn');
-submitBookBtn.addEventListener('click',()=>{
-  event.preventDefault();
+    // Get form input values
+    const title = addBookForm.title.value;
+    const author = addBookForm.author.value;
+    const pages = addBookForm.pages.value;
+    const isRead = addBookForm.isRead.checked;
+    
+    // Create new Book object
+    const newBook = new Book(title, author, pages, isRead);
+    
+    // Add new Book object to myLibrary array
+    myLibrary.push(newBook);
+    
+    // Clear the form
+    addBookForm.reset();
+    
+    // Update the display
+    displayBooks();
+    addBookForm.classList.toggle('hidden');
+  });
 
-  // Get form input values
-  const title = addBookForm.title.value;
-  const author = addBookForm.author.value;
-  const pages = addBookForm.pages.value;
-  const isRead = addBookForm.isRead.checked;
-  
-  // Create new Book object
-  const newBook = new Book(title, author, pages, isRead);
-  
-  // Add new Book object to myLibrary array
-  myLibrary.push(newBook);
-  
-  // Clear the form
-  addBookForm.reset();
-  
-  // Update the display
+
+
+
+  // call the displayBooks function to initially populate the book list
   displayBooks();
-  addBookForm.classList.toggle('hidden');
-});
-
-
-
-
-// call the displayBooks function to initially populate the book list
-displayBooks();
